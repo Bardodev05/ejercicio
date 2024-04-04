1d2a16f51702e175b472f1caac371326c1b4e41f
Bardodev05 <andersondev0526@gmail.com>
2024-04-04 10:39:02 -0500

    se añadio un print para separar los resultados

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -12,6 +12,7 @@ direrencia_cursos_promedio = 100 - curso_youtube / otros_cursos_promedio * 100
 print(f"el curso de youtube dura un {direrencia_cursos}& menos que el mas rapido")
 print(f"el curso de youtube dura un {direrencia_cursos_max}& menos que el mas lento")
 print(f"el curso de youtube dura un {direrencia_cursos_promedio}& menos que el promedio")
+print("---------------------------------")
 
 #diferencia de crudos 
 crudo_promedio = 5
@@ -24,7 +25,10 @@ tiempo_vacio_youtube = 100 - curso_youtube *1000 // crudo_youtube / 10
 #mostrando la cantida de tiempo que se remueve 
 print(f"un curso promedio elimina un {tiempo_vacio_promedio}% de tiempo vacio")
 print(f"este curso elmino el {tiempo_vacio_youtube}% de tiempo vacio")
+print("---------------------------------")
 
 
 #mostar diferencia si el curso durara 10 horas
 print(f"ver 10 horas de este curso equivale a ver {otros_cursos_promedio *100 // curso_youtube / 10} horas de otros cursos")
+print(f"ver 10 horas de este  otros curso equivale a ver {curso_youtube *100 // otros_cursos_promedio / 10} horas de otros cursos")
+print("---------------------------------")
